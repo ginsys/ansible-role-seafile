14cda86c02498a9d8d122ce2f92d7275e017a3d9
Serge van Ginderachter <serge@vanginderachter.be>
2014-06-05 21:46:26 +0200

    fix bad gitignore that excluded templates/bin
    fixes #1

---
+++ templates/bin/init_admin.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+#coding: UTF-8
+
+# {{ ansible_managed }}
+
+import check_init_admin
+import os
+import sys
+import  subprocess
+
+def main():
+    subprocess.call(["{{ seafile_latest_dir }}/seafile.sh", "start"])
+    if check_init_admin.need_create_admin():
+        check_init_admin.create_admin('{{ seafile_seahub_admin_email }}', '{{ seafile_seahub_admin_password }}')
+    else:
+        print "changed=false"
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print '\n\n\n'
+        print 'Aborted.'
+        print
+        sys.exit(1)
+    except Exception, e:
+        print
+        print 'Error happened during creating seafile admin:'
+        print e
+        print
+        sys.exit(1)
